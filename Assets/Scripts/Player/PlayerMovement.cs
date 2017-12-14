9711ad084c942ff43bdd26cbba077363ea63eb93
Yuan <nguyen.leenguyen@gmail.com>
2017-12-14 11:05:59 +0700

    Player Jump and double jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,25 +2,53 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float jumpPower = 10f;
+    [SerializeField] float secondJumpPower = 10f;
+
+    [SerializeField] Transform groundCheckPosition;
+    [SerializeField] float radius = 0.5f;
+    [SerializeField] LayerMask groundLayer;
+
     private Rigidbody myBody;
+    private bool playerJumped;
+    private bool isGrounded;
+    private bool canJumpDouble;
 
     void Awake()
     {
         myBody = GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
-    }
-
-    void FixedUpdate() // is called every 4 frames
+    void Update() // FixedUpdate() is called every 4 frames
     {
         PlayerMove();
+        PlayerGrounded();
+        PlayerJump();
     }
 
     void PlayerMove()
     {
         myBody.velocity = new Vector3(movementSpeed, myBody.velocity.y, 0f);
+    }
+
+    void PlayerGrounded()
+    {
+        isGrounded = Physics.OverlapSphere(groundCheckPosition.position, radius, groundLayer).Length > 0;
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && canJumpDouble)
+        {
+            canJumpDouble = false;
+            myBody.AddForce(new Vector3(0, secondJumpPower, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            myBody.AddForce(new Vector3(0, jumpPower, 0));
+            playerJumped = true;
+            canJumpDouble = true;
+        }
     }
 }
